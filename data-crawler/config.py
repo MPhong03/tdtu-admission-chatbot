a976982f11e251d0f22552c9abba79a8184c58a2
Dang M Phong <dangminhphong912@gmail.com>
2025-06-23 12:35:31 +0700

    [clean]: clean src

--- data-crawler/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_URL = os.getenv('BASE_URL')
-OUTPUT_DIR = os.getenv('OUTPUT_DIR', 'output')
-DETAILS_DIR = os.path.join(OUTPUT_DIR, 'details')
